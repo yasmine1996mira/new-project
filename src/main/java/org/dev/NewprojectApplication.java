7646b5ba278e3db6bf249d791a8b7c75be22398d
yasmine <yasmine.leha@gmail.com>
2020-10-05 04:45:07 +0100

    new commit

---
+++ src/main/java/org/dev/NewprojectApplication.java
@@ -0,0 +1,13 @@
+package org.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewprojectApplication.class, args);
+	}
+
+}
